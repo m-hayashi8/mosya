2a9bac5ac3209244e55f89bc9e34481d2a077815
h-hayashi <hnbyk445@yahoo.co.jp>
2021-12-18 19:55:52 +0900

    最終

--- js/script.js
+++ js/script.js
@@ -1,59 +1,60 @@
-
-//リサイズ時に画面リロード（CSS反映の為）======================
+//=====ウィンドウリサイズ（ブレークポイント超）時　画面リロード（CSS読込）=====
 $(window).on("resize", function() {
   var w = $(window).width();
+
   if (w > 1200) {  // ブレークポイント（単位：px）
     window.location = window.location;
   }
 });
 
 
-//メニュー表示================================================
+//=====メインメニュー表示=====
 $(document).ready(function(){ // 読み込まれたら
   var $window = $(this).width(); // ウィンドウ幅を取得して
   var bp = 1200;
 
   //PC版
   if($window > bp){ // 1200pxより大きかったら
-    $('.header-nav').show(); // 表示
+    $('.header-nav').show(); // メニュー表示
   }
 
   //タブレット・スマホ版
   $(function() {
-    $('.ham-btn').on("click", function(){
+    $('.ham-btn').on("click", function(){ //ハンバーガーボタンをクリックすると
   
-      $(this).toggleClass('open');
-      //クリック時の処理（メニュー展開）
-      $('.header-nav').slideToggle(200);
-    });
-  
+      $(this).toggleClass('open'); // ハンバーガーボタンを×に
+      $('.header-nav').slideToggle(200); //メインメニュー表示・非表示
+    });  
   });
 
-  // ウィンドウ　リサイズ対応
+  //=====ウィンドウ　リサイズ対応=====
   $(window).resize(function(){ // ウィンドウがリサイズされたら
     var $window = $(this).width();
     var bp = 1200;
 
-    if($window > bp){
-      $('.header-nav').show();
+    if($window > bp){  // 1200pxより大きかったら（PC版）
+      $('.header-nav').show(); //メニュー表示
       $('.ham-btn').removeClass('open'); // ×印をハンバーガーに戻す
-    }else{
-      $('.header-nav').hide();
+
+    }else{   // 上記以外（タブレット・スマホ版）
+      $('.header-nav').hide(); //メニュー非表示
       $('.ham-btn').removeClass('open'); // ×印をハンバーガーに戻す
     }
-
   });
+
 });
 
 
-//ドロップダウン===========================================================================
+//====ドロップダウン=====
 $(function() {
   //main-menu直下のli要素をマウスオーバー
   $('.main-menu').children("li").hover(function(){
-    //クリック時の処理（メニュー展開）
+    //style属性削除
+    $('.main-menu*').removeAttr("height");
+    //メニュー展開
     $(this).children("ul").stop().slideToggle(200);
-    /*$(this).children("a").toggleClass('open');*/
-    /*$(this).children('.main-menu-item-link').toggleClass('js-main-menu-item-link-back');*/
+    //style属性削除
+    $('.main-menu*').removeAttr("height");  
   });
 });
 
